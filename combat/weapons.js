d7630e24ada0b7d31a22e5e4d6f4b95d20a4f3ec
JustSadSock <136240492+JustSadSock@users.noreply.github.com>
2025-05-16 20:02:34 +0300

    Update weapons.js

--- combat/weapons.js
+++ combat/weapons.js
@@ -1,1 +1,14 @@
+// combat/weapons.js
+class Weapon {
+  /**
+   * @param {number} fireRate — выстрелов в секунду
+   * @param {number} damage   — урон за выстрел
+   */
+  constructor(fireRate, damage) {
+    this.fireRate = fireRate;
+    this.damage   = damage;
+  }
+}
 
+// базовое оружие: 1 выстрел/сек, урон 1
+window.baseWeapon = new Weapon(1, 1);
