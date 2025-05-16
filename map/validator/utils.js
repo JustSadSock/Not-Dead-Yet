a2fa591b5b8e503e1afbf124edef4a359089291d
JustSadSock <136240492+JustSadSock@users.noreply.github.com>
2025-05-16 21:21:12 +0300

    Update utils.js

--- map/validator/utils.js
+++ map/validator/utils.js
@@ -1,1 +1,9 @@
+// map/validator/utils.js
+// Вспомогательные функции
 
+export function shuffle(arr) {
+  for (let i=arr.length-1; i>0; i--) {
+    const j = Math.floor(Math.random()*(i+1));
+    [arr[i],arr[j]] = [arr[j],arr[i]];
+  }
+}
