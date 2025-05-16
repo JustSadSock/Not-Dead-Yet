2d9322a0c173fbd83a7f65f8f5a7a7048c8d3c84
JustSadSock <136240492+JustSadSock@users.noreply.github.com>
2025-05-16 21:33:34 +0300

    Update postValidate.js

--- map/validator/postValidate.js
+++ map/validator/postValidate.js
@@ -1,20 +1,26 @@
 // map/validator/postValidate.js
-// Гарантируем: каждая door имеет хотя бы 1 сосед hall
-
+/**
+ * @param {string[][]} tiles
+ * @param {number} S
+ */
 export function enforcePostValidate(tiles, S) {
-  for (let y=0;y<S;y++) for (let x=0;x<S;x++){
-    if (tiles[y][x]!=='door') continue;
-    const neigh = [[1,0],[-1,0],[0,1],[0,-1]];
-    const nHall = neigh.filter(([dx,dy])=>
-      tiles[y+dy]?.[x+dx]==='hall'
-    ).length;
-    if (nHall===0) {
-      const dir = neigh.find(([dx,dy])=>
-        tiles[y+dy]?.[x+dx]==='room'
-      );
-      if (dir) {
-        const [dx,dy] = dir, hx=x-dx, hy=y-dy;
-        if (tiles[hy]?.[hx]==='wall') tiles[hy][hx]='hall';
+  for (let y=0; y<S; y++) {
+    for (let x=0; x<S; x++) {
+      if (tiles[y][x] !== 'door') continue;
+      const neigh = [[1,0],[-1,0],[0,1],[0,-1]];
+      const nH = neigh.filter(([dx,dy]) =>
+        tiles[y+dy]?.[x+dx] === 'hall'
+      ).length;
+      if (nH === 0) {
+        // ищем направление на room
+        const dir = neigh.find(([dx,dy]) =>
+          tiles[y+dy]?.[x+dx] === 'room'
+        );
+        if (dir) {
+          const [dx,dy] = dir;
+          const hx = x - dx, hy = y - dy;
+          if (tiles[hy]?.[hx] === 'wall') tiles[hy][hx] = 'hall';
+        }
       }
     }
   }
