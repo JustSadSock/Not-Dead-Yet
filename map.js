fbc9b0bf840be5600df36b59c6e1ae9157159464
JustSadSock <136240492+JustSadSock@users.noreply.github.com>
2025-05-16 22:40:42 +0300

    Update map.js

--- map.js
+++ map.js
@@ -1,2 +1,13 @@
-// map.js
-import './map/index.js';
+// map.js, внутри GameMap
+import { carveRoomsAndHalls } from './map/rooms.js';
+import { placeAndValidateDoors } from './map/validator/doors.js';
+
+_generateChunk(cx, cy) {
+  // 1) Вырезаем комнаты и коридоры
+  const { tiles, rooms } = carveRoomsAndHalls(this.chunkSize);
+
+  // 2) Расставляем валидные двери
+  this.doors = placeAndValidateDoors(tiles, rooms, this.chunkSize);
+
+  return tiles;
+}
