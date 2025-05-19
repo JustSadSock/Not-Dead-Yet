06ff68b44ab6e13b974816906ac51d91361d105f
JustSadSock <136240492+JustSadSock@users.noreply.github.com>
2025-05-19 16:40:20 +0300

    Update utils.js

--- map/validator/utils.js
+++ map/validator/utils.js
@@ -1,8 +1,5 @@
 // map/validator/utils.js
-/** Fisher–Yates shuffle */
-export function shuffle(arr) {
-  for (let i = arr.length - 1; i > 0; i--) {
-    const j = (Math.random() * (i + 1)) | 0;
-    [arr[i], arr[j]] = [arr[j], arr[i]];
-  }
+/** включите тут любые утилиты для ваших правил */
+export function randInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
